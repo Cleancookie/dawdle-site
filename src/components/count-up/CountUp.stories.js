b16d3d27a5c9ca980bb9253b4271f199119b25b9
Alex Law <pokemon999101@hotmail.co.uk>
2024-08-15 19:21:53 +0100

    🚧 Start CountUp

---
+++ src/components/count-up/CountUp.stories.js
@@ -0,0 +1,12 @@
+import CountUp from './CountUp.vue';
+
+export default {
+  title: 'CountUp',
+  component: CountUp,
+  argTypes: {},
+  args: {},
+};
+
+export const Primary = {
+  args: {},
+};
---
+++ src/components/count-up/CountUp.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Count Up Game</h1>
+    <div class="">
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+</template>
+
+<style scoped>
+</style>
